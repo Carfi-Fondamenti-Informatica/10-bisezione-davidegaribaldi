c6ad0c00097f07fa8c7dbf72c267055722b30f4f
davidegaribaldi <100833454+davidegaribaldi@users.noreply.github.com>
2022-05-17 16:15:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,57 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+bool controllo(float a)
+{
+    float fa;
+    fa=pow(a,2)*cos(a)+1;
+    if(fa==0) {
+        return true;
+    }
+    return false;
+}
+bool funz(float a,float b)
+{
+float fa,fb;
+fa=pow(a,2)*cos(a)+1;
+fb=pow(b,2)*cos(b)+1;
+if((fa*fb)>=0)
+    {
+    return true;
+    }
+return false;
+}
 int main() {
-   return 0;
+    float a,b,x;
+    do {
+        cout << "inserire estremi" << endl;
+        cin >> a >> b;
+    }while(funz(a,b));
+while(true) {
+    x = (a + b) / 2;
+    if (controllo(x)) {
+        cout << x << endl;
+        cout << pow(x, 2) * cos(x) + 1;
+        return 0;
+    }
+    else
+    {
+    if(funz(a,b))
+    {
+     a=x;
+    }
+    else
+    {
+    b=x;
+    }
+    if(abs((b-a)/2)<pow(10,-6))
+    {
+        cout << abs((b-a)/2) << endl;
+        cout << pow(abs((b-a)/2), 2) * cos(abs((b-a)/2)) + 1;
+        return 0;
+    }
+    }
+}
+    return 0;
 }
